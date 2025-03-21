5230f32ca423666a7ce7a2784c56dd6e495a09ff
szymon adamowicz <adamowiczszymon8@gmail.com>
2025-03-21 23:42:07 +0100

    added functions for endpoints: get, add, remove and swap

--- src/data.tsx
+++ src/data.tsx
@@ -1,45 +1,84 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
 const Data: React.FC = () => {
   const [tasks, setTasks] = useState<any[]>([]);
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
-    fetch('https://localhost:7140/tasks')
+    fetch("https://localhost:7140/tasks")
       .then((response) => response.json())
       .then((data) => setTasks(data))
-      .catch((error) => console.error('Error:', error));
+      .catch((error) => console.error("Error:", error));
   }, [counter]);
 
   const handleClickAdd = () => {
-    fetch('https://localhost:7140/tasks', 
-        {method: "post", 
-        headers: { "Content-Type": "application/json" }, 
-        body: JSON.stringify({name:"new", description: "desc", isCompleted: true})})
-    .then((response) => response.json())
-    .then((data) => console.log("Task added:", data))
-    .then((data) => setCounter(counter+1))
-    .catch((error) => console.error("Error adding task:", error));
-  }
+    fetch("https://localhost:7140/tasks/add", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        name: "new",
+        description: "desc",
+        isCompleted: true,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.message);
+        setCounter(counter + 1);
+      })
+      .catch((error) => console.error("Error adding task:", error));
+  };
 
-  const handleClickRemove = () => {
-    fetch(`https://localhost:7140/tasks/${6}`, { method: "DELETE" })
-    .then((data) => setCounter(counter+1))
-    .catch((error) => console.error("Error removing task:", error));
-  }
+  const handleClickRemove = (Id: number) => {
+    fetch(`https://localhost:7140/tasks/${Id}`, { method: "DELETE" })
+      .then(async (res) => {
+        if (res.ok) {
+          const data = await res.json();
+          console.log(data.message);
+          setCounter(counter + 1);
+        }
+      })
+      .catch((error) => console.error("Error removing task:", error));
+  };
+
+  const handleSwapId = (Id1: number, Id2: number) => {
+    fetch(`https://localhost:7140/tasks/swap?task1Id=${Id1}&task2Id=${Id2}`, {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.message);
+        setCounter(counter + 1);
+      })
+      .catch((error) => console.error("Error swapping tasks:", error));
+  };
+
+  const [firstId, setFirstId] = useState("");
+  const [secondId, setSecondId] = useState("");
 
   return (
     <div className="App">
       <h1>Lista zadań</h1>
-      <button onClick={handleClickAdd}>asd</button>
-      <button onClick={handleClickRemove}>asd</button>
+      <button onClick={handleClickAdd}>Add</button>
       <ul>
-        {tasks.map((task, index) => (
-          <li key={task.id + index}>
-            {task.name} - {task.description} - {task.isCompleted ? 'true' : 'false'} - {task.id}
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <button onClick={() => handleClickRemove(task.id)}>Delete</button>
+            {task.name} - {task.description} -{" "}
+            {task.isCompleted ? "true" : "false"} - {task.id}
           </li>
         ))}
       </ul>
+      <input onChange={(e) => setFirstId(e.target.value)} value={firstId} />
+      <p>{firstId}</p>
+      <input onChange={(e) => setSecondId(e.target.value)} value={secondId} />
+      <p>{secondId}</p>
+      <button
+        onClick={() => handleSwapId(parseInt(firstId), parseInt(secondId))}
+      >
+        Swap
+      </button>
     </div>
   );
 };
