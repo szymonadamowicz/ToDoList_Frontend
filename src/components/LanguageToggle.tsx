a32c63b4b951e05e78f5eb12d33fe86d06a4c537
szymon adamowicz <adamowiczszymon8@gmail.com>
2025-04-01 11:53:13 +0200

    small language theme fix

--- src/components/LanguageToggle.tsx
+++ src/components/LanguageToggle.tsx
@@ -7,7 +7,7 @@ function LanguageToggle() {
   const { theme, changeLanguage } = useContext(ThemeContext)!;
 
   const handleChange = () => {
-    const newLang = theme?.language ? "en" : "pl";
+    const newLang = theme?.language ? "pl" : "en";
     i18n.changeLanguage(newLang);
     localStorage.setItem("lang", newLang);
     changeLanguage();
@@ -18,7 +18,7 @@ function LanguageToggle() {
       onClick={() => handleChange()}
       className="w-28 block text-center px-3 py-1 rounded bg-cyan-500 dark:bg-cyan-700 font-semibold text-white"
     >
-      {theme?.language ? "Polski" : "English"}
+      {theme?.language ? "English" : "Polski"}
     </button>
   );
 }
